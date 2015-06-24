3644265820ffd91f78317dc725128551384a6d5b
Aaron Hoffman <afhoffman@mail.fhsu.edu>
2015-06-24 15:44:37 -0500

    Cleaned up some comments in interpLib.cpp

--- interpLib.cpp
+++ interpLib.cpp
@@ -13,7 +13,7 @@ void SplineInterp::initialSetup()
 {
     ublas::vector<double> rhs(this->n);
     ublas::permutation_matrix<int> P(this->n);
-    //this function should do the stuff that is independant of which constructor is used
+
     this->A = matrixABuild(this->X, this->Y);
     this->B = matrixBBuild(this->X, this->Y);
 
@@ -61,30 +61,6 @@ SplineInterp::SplineInterp(std::string dataFile)
             }
 
             this->initialSetup();
-            //this->A = matrixABuild(this->X, this->Y);
-            //this->B = matrixBBuild(this->X, this->Y);
-
-            //ublas::permutation_matrix<int> P(this->n);
-            //ublas::vector<double> rhs(this->n);
-
-            //for (size_t i = 0; i < this->B.size(); ++i)
-            //{
-               //rhs(i) = B(i); 
-            //}
-
-            //ublas::lu_factorize(this->A,P);
-            //B = rhs;
-            //ublas::lu_substitute(A,P,rhs);
-
-            //this->a.resize( this->n - 1);
-            //this->b.resize( this->n - 1);
-
-            //for (int i = 0; i < this->n-1; ++i)
-            //{
-                //this->a(i) = rhs(i) * (X(i+1)-X(i)) - (Y(i+1) - Y(i));
-                //this->b(i) = -rhs(i+1) * (X(i+1) - X(i)) + (Y(i+1) - Y(i));
-            //}
-
 
             delete[] x;
             delete[] y;
@@ -94,13 +70,12 @@ SplineInterp::SplineInterp(std::string dataFile)
 
 SplineInterp::SplineInterp(std::vector<double> x, std::vector<double> y)
 {
-    //this should pretty much just map these into ublas::vectors
     this->n = x.size();
 
     this->X.resize(n);
     this->Y.resize(n);
 
-    for (int i = 0; i < n; ++i)
+    for (int i = 0; i < this->n; ++i)
     {
        this->X(i) = x[i]; 
        this->Y(i) = y[i]; 
@@ -118,7 +93,7 @@ double SplineInterp::interpAt(double targetValue)
     double outVal;
     for (int i = 0; i < this->n; ++i)
     {
-        //this is a feable check to see if you're passing in a value that's already in the data set.
+        //this is a not very good check to see if you're passing in a value that's already in the data set.
         //this equality should be checked with a better function for comparing doubles
         if( X(i) == targetValue )
         {

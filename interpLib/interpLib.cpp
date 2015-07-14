fb2796e40d8179a4da3ec4c7d34ba902fa6693df
C.D. Clark III <clifton.clark@gmail.com>
2015-07-14 13:16:21 -0500

    Added integral functions. Closes #4.
    Support for integrating over specified, or entire data domain.

--- interpLib/interpLib.cpp
+++ interpLib/interpLib.cpp
@@ -81,6 +81,120 @@ double SplineInterp::derivative(double x)
     return qprime;
 }
 
+double SplineInterp::integral()
+{
+  return this->integral( X(0), X(this->n-1) );
+}
+
+double SplineInterp::integral(double _a, double _b )
+{
+    // allow b to be less than a
+    double sign = 1;
+    if( _a > _b )
+    {
+      std::swap( a, b );
+      sign = -1;
+    }
+    //No extrapolation
+    _a = std::max( _a, X(0) );
+    _b = std::min( _b, X(this->n-1) );
+
+    // find the indexes that is just to the right of a and b
+    int ai = 1;
+    while( ai < this->n-1 && X(ai) < _a )
+        ai++;
+    int bi = 1;
+    while( bi < this->n-1 && X(bi) < _b )
+        bi++;
+
+    /**
+     *
+     * We can integrate the function directly using its cubic spline representation.
+     *
+     * from wikipedia:
+     *
+     * q(x) = ( 1 - t )*y_1 + t*y_2 + t*( 1 - t )( a*(1 - t) + b*t )
+     * 
+     * t = (x - x_1) / (x_2 - x_1)
+     *
+     * I = \int_a^b q(x) dx = \int_a^b ( 1 - t )*y_1 + t*y_2 + t*( 1 - t )( a*(1 - t) + b*t ) dx
+     *
+     * variable substitution: x -> t
+     *
+     * dt = dx / (x_2 - x_1)
+     * t_a = (a - x_1) / (x_2 - x_1)
+     * t_b = (b - x_1) / (x_2 - x_1)
+     *
+     * I = (x_2 - x_1) \int_t_a^t_b ( 1 - t )*y_1 + t*y_2 + t*( 1 - t )( a*(1 - t) + b*t ) dt
+     *
+     *   = (x_2 - x_1) [ ( t - t^2/2 )*y_1 + t^2/2*y_2 + a*(t^2 - 2*t^3/3 + t^4/4) + b*(t^3/3 - t^4/4) ] |_t_a^t_b
+     *
+     * if we integrate over the entire element, i.e. x -> [x1,x2], then we will have
+     * t_a = 0, t_b = 1. This gives
+     *
+     * I = (x_2 - x_1) [ ( 1 - 1/2 )*y_1 + 1/2*y_2 + a*(1 - 2/3 + 1/4) + b*(1/3 - 1/4) ]
+     *
+     */
+
+    double x_1, x_2, t;
+    double y_1, y_2;
+    double sum = 0;
+    for( int i = ai; i < bi-1; i++)
+    {
+      // x_1 -> X(i)
+      // x_2 -> X(i+1)
+      // y_1 -> Y(i)
+      // y_2 -> Y(i+1)
+      x_1 = X(i);
+      x_2 = X(i+1);
+      y_1 = Y(i);
+      y_2 = Y(i+1);
+      // X(ai) is to the RIGHT of _a
+      // X(bi) is to the RIGHT of _b, but i only goes up to bi-2 and
+      // X(bi-1) is to the LEFT of _b
+      // therefore, we are just handling interior elements in this loop.
+      sum += (x_2 - x_1)*( 0.5*(y_1 + y_2) + (1./12)*(a(i) + b(i)) );
+    }
+
+
+    // now we need to handle the area between [_a,X(ai)] and [X(bi-1),_b]
+
+
+    // [X(0),_b]
+    // x_1 -> X(bi-1)
+    // x_2 -> X(bi)
+    // y_1 -> Y(bi-1)
+    // y_2 -> Y(bi)
+    x_1 = X(bi-1);
+    x_2 = X(bi);
+    y_1 = Y(bi-1);
+    y_2 = Y(bi);
+    t   = (_b - x_1)/(x_2 - x_1);
+
+    // adding area between x_1 and _b
+    sum += (x_2 - x_1) * ( ( t - pow(t,2)/2 )*y_1 + pow(t,2)/2.*y_2 + a(bi-1)*(pow(t,2) - 2.*pow(t,3)/3. + pow(t,4)/4.) + b(bi-1)*(pow(t,3)/3. - pow(t,4)/4.) );
+
+    //
+    // [_a,X(0)]
+    // x_1 -> X(ai-1)
+    // x_2 -> X(ai)
+    // y_1 -> Y(ai-1)
+    // y_2 -> Y(ai)
+    x_1 = X(ai-1);
+    x_2 = X(ai);
+    y_1 = Y(ai-1);
+    y_2 = Y(ai);
+    t   = (_a - x_1)/(x_2 - x_1);
+
+    // subtracting area from x_1 to _a
+    sum -= (x_2 - x_1) * ( ( t - pow(t,2)/2 )*y_1 + pow(t,2)/2.*y_2 + a(ai-1)*(pow(t,2) - 2.*pow(t,3)/3. + pow(t,4)/4.) + b(ai-1)*(pow(t,3)/3. - pow(t,4)/4.) );
+
+    if( ai != bi ) // _a and _b are not in the in the same element, need to add area of element containing _a
+      sum += (x_2 - x_1)*( 0.5*(y_1 + y_2) + (1./12)*(a(ai-1) + b(ai-1)) );
+      
+    return sign*sum;
+}
+
 double SplineInterp::operator() (double x)
 {
   // don't extrapolate at all
--- testing/TestSuites/SplineInterp/SplineInterpUT.cpp
+++ testing/TestSuites/SplineInterp/SplineInterpUT.cpp
@@ -91,4 +91,65 @@ BOOST_AUTO_TEST_CASE(testSinDerivative)
     }
 }
 
+BOOST_AUTO_TEST_CASE(testSinIntegral)
+{
+    int n = 30;
+    double xmin,xmax,dx;
+    std::vector<double> x(n), y(n);
+    xmin = 0;
+    xmax = 10;
+    dx = (xmax - xmin) / (n - 1);
+
+    //set up test data
+    for (int i = 0; i < n; ++i)
+    {
+        x[i] = xmin+i*dx;
+        y[i] = sin(x[i]);
+    }
+
+    // get an interpolator
+    SplineInterp testInterp;
+    testInterp.setData(x,y);
+
+    ////test case
+    int res = 10;
+    for(int i = 0; i < res*n; i++)
+    {
+      double xx = xmin -.1 + i*dx/res;
+      if( abs(cos(xx)) > 0.01 ) // only check values much greater than zero
+        BOOST_CHECK_CLOSE( -cos(xx), testInterp.integral(0,xx)-1, 1);
+    }
+
+    for(int i = 0; i < res*n; i++)
+    {
+      double xx = xmin -.1 + i*dx/res;
+      if( abs(cos(xx)) > 0.01 ) // only check values much greater than zero
+        BOOST_CHECK_CLOSE( cos(xx), testInterp.integral(xx,0)-1, 1);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(testDefinateIntegrals)
+{
+    int n = 30;
+    double xmin,xmax,dx;
+    std::vector<double> x(n), y(n);
+    xmin = 0;
+    xmax = M_PI;
+    dx = (xmax - xmin) / (n - 1);
+
+    //set up test data
+    for (int i = 0; i < n; ++i)
+    {
+        x[i] = xmin+i*dx;
+        y[i] = sin(x[i]);
+    }
+
+    // get an interpolator
+    SplineInterp testInterp;
+    testInterp.setData(x,y);
+
+
+    BOOST_CHECK_CLOSE( 2, testInterp.integral(), 1);
+}
+
 BOOST_AUTO_TEST_SUITE_END()

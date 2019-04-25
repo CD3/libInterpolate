91f6e825ae498c5111fcde91f081d6c00501a770
C.D. Clark III <clifton.clark@gmail.com>
2019-04-24 22:31:32 -0500

    feat: added support for conan

---
+++ conanfile.py
@@ -0,0 +1,13 @@
+from conans import ConanFile, CMake, tools
+import os
+
+class libIntegrate(ConanFile):
+    generators = "cmake", "virtualenv"
+    requires = 'boost/1.69.0@conan/stable'
+    build_requires = 'cmake_installer/3.13.0@conan/stable'
+
+
+    def build(self):
+      cmake = CMake(self)
+      cmake.configure()
+      cmake.build()

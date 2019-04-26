6b062f1ef83be9bd971916c7b24544d827f70f9e
C.D. Clark III <clifton.clark@gmail.com>
2019-04-25 21:45:17 -0500

    refactor: rename conan build class

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,7 @@
 from conans import ConanFile, CMake, tools
 import os
 
-class libIntegrate(ConanFile):
+class ConanBuild(ConanFile):
     generators = "cmake", "virtualenv"
     requires = 'boost/1.69.0@conan/stable'
     build_requires = 'cmake_installer/3.13.0@conan/stable'
